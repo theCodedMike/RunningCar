4257610721a1e69affd2a116eb893e89f598f5cc
lxf <lixiaofeng1115@outlook.com>
2025-01-12 17:16:18 +0800

    init project

---
+++ Assets/Scripts/Interface/CanvasScale.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Interface
+{
+    public class CanvasScale : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
