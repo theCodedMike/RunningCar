b254baf5b046cb5f32adbe451887af4a32ec446c
lixia <lixiaofeng1115@outlook.com>
2025-02-14 01:02:03 +0800

    重命名

--- Assets/Scripts/Game/Interface/IndicatorStars.cs
+++ Assets/Scripts/Game/Interface/IndicatorStars.cs
@@ -5,18 +5,18 @@ namespace Game.Interface
 {
     public class IndicatorStars : MonoBehaviour
     {
-        public Image start1;
-        public Image start2;
-        public Image start3;
+        public Image star1;
+        public Image star2;
+        public Image star3;
 
-        public Sprite startActive;
-        public Sprite startInactive;
+        public Sprite starActive;
+        public Sprite starInactive;
 
         public void SetStar(int value)
         {
-            start1.sprite = value >= 1 ? startActive : startInactive;
-            start2.sprite = value >= 2 ? startActive : startInactive;
-            start3.sprite = value >= 3 ? startActive : startInactive;
+            star1.sprite = value >= 1 ? starActive : starInactive;
+            star2.sprite = value >= 2 ? starActive : starInactive;
+            star3.sprite = value >= 3 ? starActive : starInactive;
         }
     }
 }
