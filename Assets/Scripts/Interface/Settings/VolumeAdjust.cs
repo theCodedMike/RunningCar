b60853a943a582753509853d42fb1894cbdac326
lixiaofeng <lixiaofeng@thedream.cc>
2025-02-21 11:47:57 +0800

    音量调整逻辑类

---
+++ Assets/Scripts/Interface/Settings/VolumeAdjust.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Interface.Settings
+{
+    public enum VolumeType
+    {
+        SoundsVolume, MusicVolume
+    }
+    
+    public class VolumeAdjust : MonoBehaviour
+    {
+        public void AdjustVolume(float volume)
+        {
+            
+        }
+    }
+}
